57e517190aedf035160e5526005d3205a3781e1d
Sanket22g <sanketghadge122@gmail.com>
2026-03-28 19:15:02 +0530

    /

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,4 +1,10 @@
 from mangum import Mangum
+import os
+import sys
+
+SRC_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
+if SRC_DIR not in sys.path:
+	sys.path.append(SRC_DIR)
 
 from ai_agent_app_for_summary.api import app
 
